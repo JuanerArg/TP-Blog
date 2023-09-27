3ce4460290dd78fc5f496e8e5f50676ab0a18d3c
Juaner <Juaner@gmail.com>
2023-09-26 22:17:57 -0300

    Correccion del uso de ReactRouter(?)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import PostCreator from './PostCreator';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Link } from 'react-router-dom';
 function App() {
   return (
     <>
       <h1>Hola Mundo</h1>
       <Router>
-        <Route path="/PostCreator" element={<PostCreator />}></Route>
+        <Link to="/PostCreator">Crear Post</Link>
       </Router>
     </>
   );
