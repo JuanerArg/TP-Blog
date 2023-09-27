2e6445c5eac63c3e3bcbd26ebcfc710ed548ecd9
Juaner <Juaner@gmail.com>
2023-09-27 00:21:58 -0300

    Correccion del uso de ReactRouter(?)

---
+++ src/PostPage.jsx
@@ -0,0 +1,12 @@
+import ComentCreator from "./ComentCreator";
+
+const PostPage = () => {
+
+    return (
+        <>
+            <h1>Aca van los posts</h1>
+            <ComentCreator />
+        </>
+
+    )
+}
