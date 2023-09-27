21fd3be5c906852deca22335ca3c1f1c328620d6
Juaner <Juaner@gmail.com>
2023-09-26 22:50:56 -0300

    Correccion del uso de ReactRouter(?)

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import { ReactDOM } from "react";
 import App from "./App";
 import PostCreator from "./PostCreator"
 
