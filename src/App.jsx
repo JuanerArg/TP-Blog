d8882bf31f7b20609a6cd7b44a01ad7bb53fecbe
Juaner <Juaner@gmail.com>
2023-09-27 00:32:18 -0300

    Debbuging

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import PostCreator from './PostCreator';
+import PostPage from './PostPage';
 import { Router, Route, Link } from 'react-router-dom';
 function App() {
   return (
     <>
       <h1>Hola Mundo</h1>
       <Link to="/PostCreator">Crear Post</Link>
-      <Link to="/PostPage">Aca iria el post(vamos a tener que hacer que esto se genere con un map o algo por el estilo)</Link>
+      <Link to="/PostPage">Aca iria el post vamos a tener que hacer que esto se genere con un map o algo por el estilo</Link>
     </>
     //Cuando hagas el lugar para los post inclui el ComentCreator adentro de los Posts
   );
