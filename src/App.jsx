351df063201ea2738001bb5a21e3083910d137f7
Juaner <Juaner@gmail.com>
2023-09-26 21:36:54 -0300

    Agrego El creador de Post Como un anchor con un href

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <>
       <h1>Hola Mundo</h1>
+      <a href='./PostCreator'>Crear Post</a>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
+import PostCreator from "./PostCreator"
 
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
@@ -9,7 +10,8 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 const router = createBrowserRouter([
-  { path: "/", element: <App /> }
+  { path: "/", element: <App /> },
+  { path: "/PostCreator", element: <PostCreator /> }
 ]);
 
 root.render(
