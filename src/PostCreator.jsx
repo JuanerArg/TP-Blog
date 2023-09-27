c2104928bdaba3951bf6915eaa5510fc46c4d1bb
Your Name <you@example.com>
2023-09-27 10:35:13 -0300

    JKSJDKAJ

--- src/PostCreator.jsx
+++ src/PostCreator.jsx
@@ -1,55 +1,54 @@
-import { useState } from "react"
+import { useState } from "react";
 const PostCreator = () => {
-    const [Post, setPost] = useState([]);
-    const [Titulo, setTitulo] = useState("");
-    const [Parrafo, setParrafo] = useState("");
+  const [Post, setPost] = useState([]);
+  const [Titulo, setTitulo] = useState("");
+  const [Parrafo, setParrafo] = useState("");
 
-    const handleTitle = (e) => {
-        setTitulo(e.target.value)
-    }
+  const handleTitle = (e) => {
+    setTitulo(e.target.value);
+  };
 
-    const handleParagraph = (e) => {
-        setParrafo(e.target.value)
-    }
+  const handleParagraph = (e) => {
+    setParrafo(e.target.value);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const nuevoPost = { titulo: Titulo, parrafo: Parrafo };
-        setPost([...Post, nuevoPost]);
-        setTitulo("");
-        setParrafo("");
-        console.log(Post)
-    }
-    return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <input
-                    value={Titulo}
-                    onChange={handleTitle}
-                    type="text"
-                    placeholder="Title"
-                >
-                </input>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const nuevoPost = { titulo: Titulo, parrafo: Parrafo };
+    setPost([...Post, nuevoPost]);
+    setTitulo("");
+    setParrafo("");
+    console.log(Post);
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={Titulo}
+          onChange={handleTitle}
+          type="text"
+          placeholder="Title"
+        ></input>
 
-                <input
-                    value={Parrafo}
-                    onChange={handleParagraph}
-                    type="text"
-                    placeholder="Paragraph"
-                >
-                </input>
-                <input type="submit" value="ingresar" hidden />
-            </form>
+        <input
+          value={Parrafo}
+          onChange={handleParagraph}
+          type="text"
+          placeholder="Paragraph"
+        ></input>
+        <input type="submit" value="ingresar" hidden />
+      </form>
 
-            <ul>
-                {
-                    Post.map((post, i) => (
-                        <li key={i}><strong>{post.titulo}</strong>{post.parrafo}</li>
-                    ))
-                }
-            </ul>
-        </>
-    )
-}
+      <ul>
+        {Post.map((post, i) => (
+          <li key={i}>
+            <strong>{post.titulo}</strong>
+            {post.parrafo}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
-export default PostCreator
+export default PostCreator;
